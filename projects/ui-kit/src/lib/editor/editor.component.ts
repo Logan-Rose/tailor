51f390b15456c98c05347fb0b107a4d858259552
Logan-Rose <logan.rose0329@gmail.com>
2023-01-24 22:15:22 -0500

    refactored to remove unnecessary api

---
+++ projects/ui-kit/src/lib/editor/editor.component.ts
@@ -0,0 +1,152 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  CompactType,
+  GridsterConfig,
+  GridsterItem,
+  GridType,
+} from 'angular-gridster2';
+import { v4 as uuidv4 } from 'uuid';
+import {MatDialog} from '@angular/material/dialog';
+
+import { SaveDialogComponent } from '../save-dialog/save-dialog.component';
+
+@Component({
+  selector: 'tailor-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss'],
+})
+export class EditorComponent implements OnInit {
+  options: GridsterConfig;
+  dashboard: Array<GridsterItem>;
+  background = true;
+  constructor(public dialog: MatDialog) {
+    console.log('editor constructed')
+    this.options = {}
+    this.dashboard = []
+  }
+  // add Drop to add for saved components
+  ngOnInit() {
+    this.options = {
+      gridType: GridType.Fit,
+      compactType: CompactType.None,
+      margin: 5,
+      minCols:20,
+      maxCols:20,
+      minRows:20,
+      maxRows:20,
+      useTransformPositioning: true,
+      fixedColWidth: 105,
+      fixedRowHeight: 105,
+      scrollSensitivity: 10,
+      scrollSpeed: 20,
+      draggable: {
+        enabled: true
+      },
+      resizable: {
+        enabled: true
+      },
+      pushItems: true,
+      pushDirections: { north: true, east: true, south: true, west: true },
+      itemChangeCallback: this.itemChange,
+      itemResizeCallback: this.itemResize,
+    };
+
+     this.dashboard = [
+       {id: uuidv4(), cols: 10, rows: 10, y: 0, x: 0, title: 'Logan Rose', subtitle: 'Software Engineer', align: "center", titleSize:12, subtitleSize:12, listItems: ['hello','goodbye'], dragEnabled: true},
+       {id: uuidv4(), cols: 2, rows: 2, y: 0, x: 2, title: 'Coveo', subtitle: 'Software Developer Consultant', bodyType:'chip-list',listItems:['hello','goodbye','javascript','python','angular','nestjs','postgresql'], align:"left", titleSize:12, subtitleSize:12, dragEnabled: true}
+     ];
+  }
+
+  itemChange(){
+    console.log('ITEM CHANGED')
+    console.log(this)
+  }
+  itemResize(){
+    console.log("Item Resized")
+  }
+
+  removeItem(item: any) {
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  addTile(){
+    console.log('add tile')
+    console.log(uuidv4())
+    let layout = { id: uuidv4(), x: 0, y: 0, cols:9, rows: 3, title: 'New Tile', body: 'Double click to modify', titleSize:18, dragEnabled: true}
+    console.log(layout)
+    this.dashboard.push(layout);
+  }
+  save(){
+    console.log('Save')
+    console.log(this.dashboard)
+    const dialogRef = this.dialog.open(SaveDialogComponent, {
+      data: this.dashboard
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+    
+  }
+  load(){
+    console.log('Load')
+  }
+  print(){
+    console.log(this.dashboard)
+  }
+
+  lock(){
+    console.log('Lockign')
+    if(this.options.draggable){
+      if(this.options.draggable.enabled != undefined){
+        console.log(this.options.draggable.enabled)
+        this.options.draggable.enabled = !this.options.draggable.enabled
+      }
+      if (this.options.api && this.options.api.optionsChanged) {
+        this.options.api.optionsChanged();
+      }
+    }
+  }
+
+  updateRow(num: number){
+    console.log(num)
+  }
+  updateCol(num: number){
+    console.log(num)
+  }
+
+  duplicateTile(e: any){
+    const duplicatedTile = Object.assign({}, e)
+    duplicatedTile.id = uuidv4();
+    this.dashboard.push(duplicatedTile);
+    console.log(this.dashboard)
+  }
+
+  deleteTile(tileId: string){
+    console.log(this.dashboard)
+    console.log(tileId)
+    this.dashboard = this.dashboard.filter(x => x['id'] !== tileId)
+    console.log(this.dashboard)
+  }
+  saveTile(tile: any){
+    console.log(tile)
+    this.dashboard = this.dashboard.filter(x => x['id'] !== tile['id'])
+    this.dashboard.push(tile)
+    console.log(this.dashboard)
+    
+  }
+  toggleEditMode(tile: any){
+      tile.editing = !tile.editing
+      tile.editMode = ! tile.editMode
+      tile.dragEnabled = !tile.dragEnabled
+      console.log(this.dashboard)
+  }
+  toggleBackground(){
+    this.background = !this.background
+  }
+
+  signIn(){
+    console.log('signing in')
+  }
+}
---
+++ projects/ui-kit/src/lib/ui-kit.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditorComponent } from './editor/editor.component';
+import { GridsterModule } from 'angular-gridster2';
+import { TileComponent } from './tile/tile.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { SaveDialogComponent } from './save-dialog/save-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+  declarations: [
+    TestComponent,
+    EditorComponent,
+    TileComponent,
+    SaveDialogComponent
+  ],
+  imports: [
+    CommonModule,
+    GridsterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSliderModule,
+    MatCardModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatIconModule,
+    MatExpansionModule,
+    ReactiveFormsModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    TestComponent,
+    EditorComponent, 
+    TileComponent
+  ]
+})
+export class UiKitModule { }
